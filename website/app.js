1de8f961c4f62996ead77785f0a074e5a34e1f5d
Mahmoud Mamdouh <mahmoud.khalil1072@gmail.com>
2021-05-13 20:29:01 +0200

    init

---
+++ website/app.js
@@ -0,0 +1,71 @@
+/* Global Variables */
+const generate = document.getElementById('generate');
+const baseUrl = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+const apiKey = '&APPID=012f4d0b39937e045bce14f0621f077e';
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+generate.addEventListener('click', function(event) {
+
+  let zip = document.getElementById('zip').value;
+  let feelings = document.getElementById('feelings').value;
+
+  if(!zip) {
+    alert('Please, enter a zip code');
+  } else {
+    getWeather(baseUrl, zip, apiKey)
+      .then(function (data) { // data is an object
+        postData('/addData', {
+          date: newDate,
+          temp: data.main.temp,
+          feelings: feelings
+        })
+
+        updateUI();
+      })
+  }
+});
+
+//post data
+const postData = async (url = '', data = {}) => {
+  const res = await fetch(url, {
+    method: 'POST',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+  try {
+    return;
+  } catch (error) {
+    console.log("postData error", error)
+  }
+}
+
+
+//get API data
+const getWeather = async (url, zip, apiKey) => {
+  const req = await fetch(url + zip + apiKey);
+  try {
+    const data = await req.json();
+    return data;
+  } catch (error) {
+    console.log("get weather error", error);
+  }
+}
+
+
+const updateUI = async (url = '') => {
+  const req = await fetch('/all');
+  try {
+    const allData = await req.json();
+
+    document.getElementById('date').innerText = allData.date;
+    document.getElementById('temp').innerText = allData.temp;
+    document.getElementById('content').innerText = `Your feeling: ${allData.feelings}`;
+  } catch (error) {
+    console.log("update UI error", error);
+  }
+}
